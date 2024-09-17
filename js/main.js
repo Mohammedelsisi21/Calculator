3c0647ab50b0ba3b4ec73a85b9ec80a25fde4c0d
Mohamed Elsisi <mohammedelsisi21@gmail.com>
2024-09-17 23:17:49 +0300

    added a design

--- js/main.js
+++ js/main.js
@@ -6,4 +6,21 @@ window.addEventListener("load", function () {
             preloader.remove();
         }, 3000);
 });
-// End load
+const parentElement = document.querySelector('.parent');
+const openButton = document.querySelector('.open-btn');
+const icon = openButton.querySelector('.icon-lok'); // هنا نستخدم openButton للوصول إلى الأيقونة داخل الزر
+openButton.addEventListener('click', () => {
+if (parentElement.classList.contains('animate')) {
+    parentElement.classList.remove('animate');
+    parentElement.classList.add('close');
+    icon.classList.remove('fa-lock-open');
+    icon.classList.add('fa-unlock');
+    console.log(icon)
+} else {
+    parentElement.classList.remove('close');
+    parentElement.classList.add('animate');
+    icon.classList.remove('fa-unlock');
+    icon.classList.add('fa-lock-open');
+    console.log(icon)
+}
+});
